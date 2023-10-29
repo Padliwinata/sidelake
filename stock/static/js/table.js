e346a7ca8b5185825c5e8fa4cb1867905218c8d8
internQA-SalsaArifahZakkiya <salsaarifahzakkiya@gmail.com>
2023-10-29 17:52:06 +0700

    update

--- stock/static/js/table.js
+++ stock/static/js/table.js
@@ -1,8 +1,8 @@
 $(document).ready( function () {
     $('#example').DataTable({
         columnDefs: [
-            { orderable: false, className: 'reorder', targets: 5},
-            { orderable: false, className: 'reorder', targets: 6},
+            { orderable: false, className: 'reorder', targets: 7},
+            { orderable: false, className: 'reorder', targets: 8},
             ],
         lengthMenu: [[5,10,25,50,-1], [5,10,25,50, "All"]]
     });
