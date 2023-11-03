2f2688b889914e91e8d0a728b1cb37eced23150f
internQA-SalsaArifahZakkiya <salsaarifahzakkiya@gmail.com>
2023-11-03 14:39:19 +0700

    update

--- sidelake/urls.py
+++ sidelake/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stock', include('stock.urls'))
+    path('stock', include('stock.urls')),
+    path('', lambda: redirect('stock-index'))
 ]
