14be15f08f4d25800fdf3b29a49ccfd734c58112
rpadliwinata <rpadliwinata@gmail.com>
2023-11-22 18:10:35 +0700

    update

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
@@ -9,4 +12,17 @@ def login(request):
         return render(request, 'stock/login.html')
 
 def handle_login(request):
-    pass
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are logged in now as {username}")
+                return redirect('stock-index', {'username': username})
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
